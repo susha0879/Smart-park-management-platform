cc4d82770f2976650ab98b29cb0882c044038914
susha <1092036581@qq.com>
2022-09-02 12:26:17 +0800

    崩溃了

--- src/views/operation.vue
+++ src/views/operation.vue
@@ -1,11 +1,23 @@
 <!-- 运营管理 -->
 <template>
-    <div>
-        运营测试
-
-    </div>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px" name="asideMenu">
+        <opMenu></opMenu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
 </template>
-<script setup>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import opMenu from '@/components/operationMenu';
+export default defineComponent({
+      name:"operation",
+      components:{
+        opMenu
+      },
+});
 </script>
 
 <style scoped>
